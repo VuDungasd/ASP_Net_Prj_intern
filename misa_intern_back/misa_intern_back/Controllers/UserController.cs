eb2604a540356f04380a298aff01595745e11519
VuDungasd <91481784+VuDungasd@users.noreply.github.com>
2024-08-08 01:51:23 +0700

    update code
    update code front + backend asp.net

--- misa_intern_back/misa_intern_back/Controllers/UserController.cs
+++ misa_intern_back/misa_intern_back/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using misa_intern_back.Data;
 using misa_intern_back.Models;
 
@@ -34,12 +35,90 @@ public class UserController : ControllerBase
         return Ok(user);
     }
 
+    // [HttpPost]
+    // public IActionResult CreateUser([FromBody] User user)
+    // {
+    //     _appDbContext.Users.Add(user);
+    //     _appDbContext.SaveChanges();
+    //     return CreatedAtAction(nameof(GetById), new { id = user.Id }, user);
+    // }
+    
     [HttpPost]
-    public IActionResult CreateUser([FromBody] User user)
+    public async Task<ActionResult<User>> PostUser(User user)
     {
         _appDbContext.Users.Add(user);
-        _appDbContext.SaveChanges();
+        await _appDbContext.SaveChangesAsync();
+
         return CreatedAtAction(nameof(GetById), new { id = user.Id }, user);
     }
     
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutUser(int id, User user)
+    {
+        // if (id != user.Id)
+        // {
+        //     return BadRequest("User ID mismatch");
+        // }
+
+        var existingUser = await _appDbContext.Users.FindAsync(id);
+        if (existingUser == null)
+        {
+            return NotFound();
+        }
+
+        existingUser.code = user.code;
+        existingUser.userName = user.userName;
+        existingUser.birthday = user.birthday;
+        existingUser.gender = user.gender;
+        existingUser.position = user.position;
+        existingUser.personalNumber = user.personalNumber;
+        existingUser.create_date = user.create_date;
+        existingUser.department = user.department;
+        existingUser.issued_By = user.issued_By;
+        existingUser.address = user.address;
+        existingUser.mobilePhone = user.mobilePhone;
+        existingUser.landline = user.landline;
+        existingUser.email = user.email;
+        existingUser.bacnkCode = user.bacnkCode;
+        existingUser.bankName = user.bankName;
+        existingUser.bankBranch = user.bankBranch;
+
+        try
+        {
+            await _appDbContext.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!UserExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        var user = await _appDbContext.Users.FindAsync(id);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        _appDbContext.Users.Remove(user);
+        await _appDbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool UserExists(int id)
+    {
+        return _appDbContext.Users.Any(e => e.Id == id);
+    }
 }
